67156d257f60870986f4f81e9b7eeb5bd63f1a7d
Alexander Hale <thalexander@openmailbox.org>
2016-11-02 12:48:25 +0000

    Add citation model

--- citations/models.py
+++ citations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Citation(models.Model):
+    """A citation created by a user"""
+    title = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
+    note = models.TextField()
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        if len(title) > 50:
+            return self.title[:50] + "..."
+        else:
+            return self.title
