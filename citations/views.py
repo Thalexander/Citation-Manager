401b595017e17d5cb10a8c391e546c0c124442eb
Alexander Hale <thalexander@openmailbox.org>
2016-11-02 23:12:33 +0000

    Add citations page

--- citations/views.py
+++ citations/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import Citation
+
 def index(request):
     """The home page for Citation Manager."""
     return render(request, 'citations/index.html')
+
+def citations(request):
+    """Show all citations."""
+    citations = Citation.objects.all()
+    context = {'citations': citations}
+    return render(request, 'citations/citations.html', context)
