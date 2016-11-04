1e3542f302b1fdebfc35c6096c1b78720e9e5ac4
Alexander Hale <thalexander@openmailbox.org>
2016-11-04 03:31:12 +0000

    Fix typo in settings

--- citation_manager/settings/prod.py
+++ citation_manager/settings/prod.py
@@ -2,5 +2,6 @@ from citation_manager.settings.common import *
 
 INSTALLED_APPS += ['storages']
 AWS_STORAGE_BUCKET_NAME = "cm-static-files"
-STATICFILES_STORAGE = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = S3_URL
