3f7b9ebccb22e77b892b31ab830648f1fa884abc
Alexander Hale <thalexander@openmailbox.org>
2016-11-04 04:44:11 +0000

    Set up database on production

--- citation_manager/settings/common.py
+++ citation_manager/settings/common.py
@@ -68,13 +68,6 @@ WSGI_APPLICATION = 'citation_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -93,7 +86,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
